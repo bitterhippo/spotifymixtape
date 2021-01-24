cdc1ccfed6dcd143f508f48f8621a50f6e747768
unknown <bitterhippo@gmail.com>
2021-01-24 09:54:57 +0900

    strange buttons

--- src/Subcomponents/NoteBox.js
+++ src/Subcomponents/NoteBox.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../App.css';
 
-
-
-
-
 function NoteBox() {
 
   return (
-    <div id='notebox'>
+    <div style={styles.noteBox}>
       NoteBox
     </div>
   )
 }
 
-
+const styles = {
+  noteBox : {
+    position: `absolute`,
+    marginTop: `30px`,
+    marginLeft: `60px`,
+    width: 250,
+    height: 250,
+    backgroundColor: `green`
+  }
+}
 
 
 
---
+++ src/Subcomponents/Player.js
@@ -0,0 +1,57 @@
+import React from "react";
+import '../App.css';
+
+const Player = props => {
+  return (
+    <div style={styles.player}>
+      <div style={styles.shuffler}>
+        <figure>
+          <button name="play"></button>
+        </figure>
+        <div style={styles.shuffler2}>
+          Shuffle Play
+        </div>
+      </div>
+      <div style={styles.toggle}>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider round"></span>
+        </label>
+        <div style={styles.toggle2}>
+          Show Liked Songs
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  player: {
+    display: "inline-flex",
+    justifyContent: "space-between",
+    position: "absolute",
+    width: "90%",
+    marginLeft: 20,
+    bottom: "30%",
+    height: 45
+  },
+  shuffler: {
+    display: "inline-flex",
+  },
+  shuffler2: {
+    marginLeft: 11,
+    marginTop: 15,
+  },
+  toggle: {
+    display: "inline-flex",
+    flexDirection: "row-reverse",
+    marginTop: 7,
+  },
+  toggle2: {
+    fontSize: 12,
+    marginTop: 11,
+    marginRight: 5
+  }
+};
+
+export default Player;
