4c1051a36abf9adba9531a9e4ab81d172a219f94
Noah <bitterhippo@gmail.com>
2021-02-04 10:23:51 +0900

    Update App.js
    Cleaning up code base

--- src/App.js
+++ src/App.js
@@ -14,24 +14,6 @@ class App extends React.Component {
     this.state = {
       selected: []
     }
-
-    this.onClick = this.onClick.bind(this);
-  }
-
-  //This function is currently quite verbose - please use a destructoring assignment to increase the readability. 
-  onClick(event) {
-    if (this.state.selected.includes(event.target.getAttribute('name')) === false) {
-      this.setState({
-        selected: [...this.state.selected, event.target.getAttribute('name')]
-      })
-    }
-    else {
-      let removedItem = this.state.selected.filter(x => x !== event.target.getAttribute('name'))
-      console.log(removedItem)
-      this.setState({
-        selected: [...removedItem]
-      })
-    }
   }
 
   render() {
