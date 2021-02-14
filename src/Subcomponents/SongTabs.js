615a2fb8d1b07011be9e8d6048e2bfc2b1f93f6f
Noah <bitterhippo@gmail.com>
2021-02-14 22:11:15 +0900

    Update SongTabs.js

--- src/Subcomponents/SongTabs.js
+++ src/Subcomponents/SongTabs.js
@@ -4,7 +4,7 @@ const SongTabs = props => {
   return (
     <div style={styles.container}>
       <div>
-        <img style={styles.img} src={props.props.img}></img>
+        <img alt={styles.img} style={styles.img} src={props.props.img}></img>
       </div>
       <div style={styles.textBox}>
         <p style={styles.text}> {props.props.text} </p>
